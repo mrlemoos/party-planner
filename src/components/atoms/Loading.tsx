3c0cf74c81ad22ba50867311ebdcade9a515cc8a
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-09 12:24:54 -0300

    Create Loading component ⭕️

---
+++ src/components/atoms/Loading.tsx
@@ -0,0 +1,42 @@
+import { type JSX, type HTMLAttributes } from "react";
+
+import cls from "classnames";
+
+import Logo from "./Logo";
+
+interface LoadingProps extends HTMLAttributes<HTMLElement> {
+  hasPartyEmoji?: boolean;
+  emojiClassName?: string;
+  isLogoMinimum?: boolean;
+  logoClassName?: string;
+}
+
+export default function Loading({
+  className,
+  hasPartyEmoji,
+  emojiClassName,
+  children = "Loading",
+  isLogoMinimum = false,
+  logoClassName,
+  ...props
+}: LoadingProps): JSX.Element {
+  return (
+    <div className={cls("flex flex-col justify-center items-center", className)} {...props}>
+      {hasPartyEmoji && (
+        <div
+          className={cls(
+            "motion-safe:animate-bounce motion-reduce:hidden text-2xl pointer-events-none",
+            emojiClassName
+          )}
+        >
+          🎉
+        </div>
+      )}
+      <div className="flex items-center pointer-events-none">
+        <Logo className={cls("animate-bounce mr-3", logoClassName)} isMinimum={isLogoMinimum} />
+        <span className="text-xl">{children}</span>
+        <span className="animate-pulse text-3xl">...</span>
+      </div>
+    </div>
+  );
+}
