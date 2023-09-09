e8ad8b726817494d9222ea9b371dd13ecca6d970
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-09 12:28:00 -0300

    Create AuthPage component 📬

---
+++ src/components/organisms/AuthPage.tsx
@@ -0,0 +1,23 @@
+import { Fragment, type ReactNode } from "react";
+
+import Link from "next/link";
+
+import Logo from "@root/components/atoms/Logo";
+import GradientContainer from "@root/components/molecules/GradientContainer";
+
+interface AuthPageProps {
+  children: ReactNode;
+}
+
+export default function AuthPage({ children }: AuthPageProps): JSX.Element {
+  return (
+    <Fragment>
+      <div className="fixed right-10 top-8">
+        <Link href="/" target="_self">
+          <Logo isInteractive={true} />
+        </Link>
+      </div>
+      <GradientContainer>{children}</GradientContainer>
+    </Fragment>
+  );
+}
