20a9d2e5077e38479818959ed238989de4d85187
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-09 12:27:30 -0300

    Create GradientContainer component 🌵

---
+++ src/components/molecules/GradientContainer.tsx
@@ -0,0 +1,21 @@
+import { type ReactNode } from "react";
+
+import cls from "classnames";
+
+interface GradientContainerProps {
+  children: ReactNode;
+}
+
+export default function GradientContainer({ children }: GradientContainerProps): JSX.Element {
+  return (
+    <div
+      className={cls(
+        "flex flex-col justify-center items-center bg-gradient-to-tr from-blue-400 to-purple-400",
+        "dark:from-blue-800 dark:to-purple-800"
+      )}
+      style={{ width: "100vw", height: "100vh" }}
+    >
+      {children}
+    </div>
+  );
+}
