c07e34cabd8e5fb5423405c0fff3b866ef628115
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-09 12:22:29 -0300

    Create Button component 🎨

---
+++ src/components/atoms/Button.tsx
@@ -0,0 +1,19 @@
+import { type ButtonHTMLAttributes, type ReactElement } from "react";
+
+import cls from "classnames";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = ({ children, className, ...props }: ButtonProps): ReactElement => (
+  <button
+    className={cls(
+      "flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 transition-all",
+      className
+    )}
+    {...props}
+  >
+    {children}
+  </button>
+);
+
+export default Button;
