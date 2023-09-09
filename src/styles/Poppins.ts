4ba6be54668dc13a5afc09788b3ab030dff3a023
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-09 12:34:09 -0300

    Create global styles and font definition 🌎

---
+++ src/styles/Poppins.ts
@@ -0,0 +1,8 @@
+import { Poppins as Poppins$ } from "next/font/google";
+
+const Poppins = Poppins$({
+  weight: ["600", "500"],
+  subsets: ["latin"],
+});
+
+export default Poppins;
---
+++ src/styles/RobotoMono.ts
@@ -0,0 +1,9 @@
+import { Roboto_Mono } from "next/font/google";
+
+const RobotoMono = Roboto_Mono({
+  subsets: ["latin-ext"],
+  display: "swap",
+  weight: ["400", "700"],
+});
+
+export default RobotoMono;
