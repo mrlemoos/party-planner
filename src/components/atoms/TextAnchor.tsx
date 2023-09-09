92ff34e7af5cf779e383b3a900d38096d4c4b760
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-09 12:26:01 -0300

    Create TextAnchor component ⚓️

---
+++ src/components/atoms/TextAnchor.tsx
@@ -0,0 +1,27 @@
+import { type AnchorHTMLAttributes, type JSX } from "react";
+
+import cls from "classnames";
+import Link, { type LinkProps } from "next/link";
+
+type TextAnchorProps = AnchorHTMLAttributes<HTMLAnchorElement> & LinkProps;
+
+export default function TextAnchor({
+  children,
+  className,
+  ...props
+}: TextAnchorProps): JSX.Element {
+  return (
+    <Link
+      className={cls(
+        "decoration-transparent font-medium px-2 py-1",
+        "text-purple-900 border-b-transparent border-b-2",
+        "hover:border-b-purple-300",
+        "dark:text-purple-200",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+}
