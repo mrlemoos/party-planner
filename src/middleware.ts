44d539deeebafbd7c181b7126183840905362c2a
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-09 12:19:30 -0300

    Initialise authentication middleware with ClerkJS

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes: ["/", "/sign-in", "/sign-up", "/parties/:partyId/boards", "/api/:path*"],
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/(api)(.*)", "/parties/create"],
+};
