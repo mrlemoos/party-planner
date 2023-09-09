0a34eda593fbbc984185a3b1b4d3beb124f7668d
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-09 12:35:03 -0300

    Create services layer with createParty() and fetchParty() functions

---
+++ src/services/createParty.ts
@@ -0,0 +1,28 @@
+import type CreatePartyResponseContract from "@root/contracts/parties/create/CreatePartyResponseContract";
+import getEnv from "@root/util/getEnv";
+
+const serverURL = getEnv("SERVER_URL");
+
+interface CreatePartyOptions {
+  token: string;
+}
+
+export default async function createParty({
+  token,
+}: CreatePartyOptions): Promise<CreatePartyResponseContract> {
+  if (!token) {
+    throw new Error("createParty(): You must be logged in to create a party.");
+  }
+
+  const meta = await fetch(`${serverURL}/parties`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const response = await meta.json();
+
+  return response;
+}
---
+++ src/services/fetchParty.ts
@@ -0,0 +1,30 @@
+import type GetPartyByIdResponseContract from "@root/contracts/parties/by-party-id/GetPartyByIdResponseContract";
+import getEnv from "@root/util/getEnv";
+
+const serverURL = getEnv("SERVER_URL");
+
+interface FetchPartyByIdOptions {
+  token: string;
+}
+
+export default async function fetchParty({ token }: FetchPartyByIdOptions): Promise<GetPartyByIdResponseContract> {
+  if (!token) {
+    throw new Error("fetchParty(): You must be logged in to fetch a party.");
+  }
+
+  const meta = await fetch(`${serverURL}/parties`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  const response = await meta.json();
+
+  if (Object.values(response).length === 0) {
+    return null;
+  }
+
+  return response;
+}
