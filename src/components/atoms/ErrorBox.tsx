1e251b4e05b2fba0bb7244b4ddfb66bbece308ac
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-09 12:23:23 -0300

    Create ErrorBox component 🚨

---
+++ src/components/atoms/ErrorBox.tsx
@@ -0,0 +1,33 @@
+import { type JSX } from "react";
+
+import cls from "classnames";
+import Link from "next/link";
+
+interface ErrorBoxProps {
+  isContainer?: boolean;
+}
+
+export default function ErrorBox({ isContainer }: ErrorBoxProps): JSX.Element {
+  return (
+    <div
+      className={cls(
+        "flex flex-col flex-1 justify-center items-center rounded-md bg-red-200 dark:bg-red-500",
+        {
+          "container mx-auto": isContainer,
+        }
+      )}
+    >
+      <h1 className="font-medium text-xl text-red-950 dark:text-red-200">🚨 An error occurred</h1>
+
+      <h2 className="text-gray-500 dark:text-gray-900">Please try again later</h2>
+
+      <Link
+        href="/"
+        target="_self"
+        className="decoration-transparent px-3 py-2 bg-white rounded-md border-[1px] text-red-900 shadow-sm hover:shadow-md"
+      >
+        Back home
+      </Link>
+    </div>
+  );
+}
