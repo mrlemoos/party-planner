71ba4ca41e1e6ae78ec4a412e989e76f982b6b41
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-09 12:23:14 -0300

    Create EllipsisLoading component ⚙️

---
+++ src/components/atoms/EllipsisLoading.tsx
@@ -0,0 +1,22 @@
+import { HTMLAttributes } from "react";
+
+import cls from "classnames";
+
+type EllipsisLoadingProps = Omit<HTMLAttributes<HTMLDivElement>, "children"> & {
+  children?: never;
+  containerClassName?: string;
+};
+
+export default function EllipsisLoading({
+  className,
+  containerClassName,
+  ...props
+}: EllipsisLoadingProps): JSX.Element {
+  return (
+    <div className={cls("flex justify-center items-center gap-x-1", containerClassName)} {...props}>
+      <div className={cls("h-2.5 w-2.5 bg-gray-400 rounded-full animate-pulse", className)} />
+      <div className={cls("h-2.5 w-2.5 bg-gray-400 rounded-full animate-pulse", className)} />
+      <div className={cls("h-2.5 w-2.5 bg-gray-400 rounded-full animate-pulse", className)} />
+    </div>
+  );
+}
