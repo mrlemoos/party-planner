cde18d06088dd6d8a14982319a6b828402450dd1
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-09 12:25:05 -0300

    Create Logo component 🛎️

---
+++ src/components/atoms/Logo.tsx
@@ -0,0 +1,51 @@
+import { type HTMLAttributes } from "react";
+
+import cls from "classnames";
+
+import Poppins from "@root/styles/Poppins";
+
+type LogoProps = HTMLAttributes<HTMLElement> & {
+  children?: never;
+  isInteractive?: boolean;
+  isMinimum?: boolean;
+};
+
+function Logo({
+  className,
+  isInteractive = false,
+  isMinimum = false,
+  ...props
+}: LogoProps): JSX.Element {
+  return (
+    <span
+      className={cls(
+        "text-sm rounded-full  py-2 cursor-pointer",
+        isMinimum
+          ? "w-8 h-8 flex items-center justify-center bg-purple-100 text-lg"
+          : "px-3 py-2 bg-purple-800 text-white relative",
+        "transition-all",
+        "dark:bg-transparent",
+        { "hover:shadow-2xl hover:bg-white hover:scale-110 hover:text-black": isInteractive },
+        "flex items-center gap-3",
+        Poppins.className,
+        className
+      )}
+      {...props}
+    >
+      {!isMinimum && (
+        <span
+          className={cls(
+            "absolute text-2xl",
+            isMinimum ? "-right-5 bottom-0" : "-top-3 -right-3",
+            "-order-1"
+          )}
+        >
+          🎉
+        </span>
+      )}
+      {isMinimum ? "🎉" : "Party Planner"}
+    </span>
+  );
+}
+
+export default Logo;
