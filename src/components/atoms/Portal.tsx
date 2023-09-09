371b2b2c1117ce2b23c6271c0bafaf2a8c9b1fe7
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-09 12:25:28 -0300

    Create Portal component 🚪

---
+++ src/components/atoms/Portal.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { type ReactNode, type ReactElement, Fragment } from "react";
+import { createPortal } from "react-dom";
+
+interface PortalProps {
+  children: ReactNode;
+  anchor?: HTMLElement | null;
+}
+
+const Portal = ({ children, anchor }: PortalProps): ReactElement => (
+  <Fragment>{createPortal(children, anchor || document.body)}</Fragment>
+);
+
+export default Portal;
