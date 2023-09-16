6aeffc0264cb05d6fed194b2e2507f51d76d8854
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-16 13:28:07 -0300

    refactor: use function instead of arrow func (=>) in getClientURL()

--- src/util/getClientURL.ts
+++ src/util/getClientURL.ts
@@ -1,5 +1,5 @@
 "use client";
 
-export default function getClientURL() {
-  return process.env.NEXT_PUBLIC_CLIENT_URL || "http://localhost:3000";
-}
+const getClientURL = () => process.env.NEXT_PUBLIC_CLIENT_URL || "http://localhost:3000";
+
+export default getClientURL;
