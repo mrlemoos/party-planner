d9b0b2bd04cb55911791712f7b96d6abd5bcb2b6
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-16 13:29:03 -0300

    refactor: accept default value in getEnv() function (optionally)

--- src/util/getEnv.ts
+++ src/util/getEnv.ts
@@ -1,8 +1,13 @@
 export default function getEnv<T extends string>(
-  name: "SUPABASE_PASSWORD" | "SUPABASE_PROJECT_URL" | "SUPABASE_API_KEY" | "SERVER_URL"
+  name: "SUPABASE_PASSWORD" | "SUPABASE_PROJECT_URL" | "SUPABASE_API_KEY" | "SERVER_URL",
+  defaultValue?: string
 ): T {
   const value = process.env[name];
   if (!value) {
+    if (typeof defaultValue === 'string') {
+      return defaultValue as T;
+    }
+
     throw new Error(`Missing environment variable: ${name}`);
   }
   return value as T;
