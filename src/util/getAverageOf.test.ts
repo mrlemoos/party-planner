d71edb54b80d78c1c1ebb8737f6833e60eeaf333
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-16 13:27:44 -0300

    test(util): create unit tests for getAverageOf() function

---
+++ src/util/getAverageOf.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from 'bun:test'
+
+import getAverageOf from "./getAverageOf";
+
+const makeSut = (...list: number[]) => {
+  const runSandbox = () => getAverageOf(list);
+  
+  return { runSandbox };
+}
+
+describe('given an array of numbers', () => {
+  test('returns the average of all the numbers', () => {
+    const { runSandbox } = makeSut(0, 10);
+    
+    const average = runSandbox();
+    
+    expect(average).toBe(5);
+  })
+})
--- src/util/getAverageOf.ts
+++ src/util/getAverageOf.ts
@@ -1,3 +1,5 @@
-export default function getAverageOf(numArray: number[]): number {
+function getAverageOf(numArray: number[]): number {
   return numArray.reduce((a, b) => a + b) / numArray.length;
 }
+
+export default getAverageOf;
