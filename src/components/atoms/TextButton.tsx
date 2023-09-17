620be46243f01e4adf72cb9a4361654954e761f7
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-17 18:57:15 -0300

    refactor: forward the TextButton ref to the native `<button>` element

--- src/components/atoms/TextButton.tsx
+++ src/components/atoms/TextButton.tsx
@@ -7,10 +7,11 @@ type TextButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 const TextButton = forwardRef<HTMLButtonElement, TextButtonProps>(({ className, children, ...props }, ref) => {
   return (
     <button
+      ref={ref}
       className={cls(
         "h-[46px] px-4 py-2 bg-transparent text-purple-900 dark:text-purple-400 motion-safe:animate-scale-in",
         "hover:bg-purple-100 dark:hover:bg-purple-900 hover:bg-opacity-10 dark:hover:bg-opacity-10 rounded-md",
-        className,
+        className
       )}
       {...props}
     >
