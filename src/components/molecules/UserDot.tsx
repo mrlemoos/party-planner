123d66c1e966b2377c1531075c7757bc982ccd26
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2023-09-17 22:09:07 -0300

    refactor: create "size" prop for <UserDot /> 📐

--- src/components/molecules/UserDot.tsx
+++ src/components/molecules/UserDot.tsx
@@ -1,14 +1,54 @@
-import { memo } from "react";
+import { type HTMLAttributes, memo, type CSSProperties } from "react";
 
-import Tooltip from "@root/components/atoms/Tooltip";
+import cls from "classnames";
+
 import useCachedUserAvatarAppearance from "@root/hooks/useCachedUserAvatarAppearance";
+import Tooltip from "@root/components/atoms/Tooltip";
 
-interface UserDotProps {
+// #region Interfaces & Types
+
+type HTMLElementAttributes = HTMLAttributes<HTMLElement>;
+type OmittedHTMLElementAttributes = Omit<HTMLElementAttributes, "size" | "aria-labelledby" | "children">;
+
+type OmittedCSSProperties = Omit<CSSProperties, "height" | "width" | "backgroundColor" | "color">;
+
+interface UserDotProps extends OmittedHTMLElementAttributes {
+  /** @ignore */
+  children?: never;
+  /**
+   * The user ID to use for the user dot. This is used to determine the
+   * background and foreground colors of the dot that will be displayed via the
+   * `getUserAvatarAppearance` function from the
+   * {@link useCachedUserAvatarAppearance}.
+   *
+   * @see {@link useCachedUserAvatarAppearance}
+   */
   userId: string;
+  /**
+   * The user display name to use for the user dot. This is used as the tooltip
+   * content.
+   */
   userDisplayName: string;
+  /**
+   * The size of the user dot. If not specified, defaults to `"medium"`.
+   *
+   * Given the size is `'small'`, the height and width will be equivalent to
+   * `4px`. Given the size is `'medium'`, the height and width will be
+   * equivalent to `8px`. Given the size is `'large'`, the height and width
+   * will be equivalent to `12px`.
+   */
+  size?: "small" | "medium" | "large";
+
+  /**
+   * The style of the user dot. This will be merged with the default style
+   * properties.
+   */
+  style?: OmittedCSSProperties;
 }
 
-const UserDot = memo(({ userId, userDisplayName }: UserDotProps) => {
+// #endregion
+
+const UserDot = memo(({ userId, userDisplayName, className, size = "medium", style = {}, ...props }: UserDotProps) => {
   const { getUserAvatarAppearance } = useCachedUserAvatarAppearance();
 
   const { backgroundColor, foregroundColor } = getUserAvatarAppearance(userId, { orDefaultsTo: "random" });
@@ -17,7 +57,20 @@ const UserDot = memo(({ userId, userDisplayName }: UserDotProps) => {
 
   return (
     <Tooltip content={userDisplayName} id={tooltipId} side='bottom'>
-      <div className='h-2 w-2 rounded-md' style={{ backgroundColor, color: foregroundColor }} aria-labelledby={tooltipId} />
+      <div
+        {...props}
+        className={cls(
+          "rounded-md",
+          {
+            "h-1 w-1": size === "small",
+            "h-2 w-2": size === "medium",
+            "h-3 w-3": size === "large",
+          },
+          className
+        )}
+        style={{ backgroundColor, color: foregroundColor, ...style }}
+        aria-labelledby={tooltipId}
+      />
     </Tooltip>
   );
 });
