4ef34cddb3f1410a2138592895d7df24166627f1
Leonardo Lemos <leo.lemos.ds@icloud.com>
2023-11-17 14:06:02 -0300

    refactor: reform the Firebase server and client access methods

--- src/services/firebase-admin-service.ts
+++ src/services/firebase-admin-service.ts
@@ -1,8 +1,6 @@
 import * as admin from 'firebase-admin'
 import { getApps } from 'firebase-admin/app'
 
-import firebaseAdminCredentials from './_/firebase-admin-credentials.json'
-
 /**
  * The service that provides access to the Firebase Admin SDK for the project to use on the server-side portion of this
  * codebase.
@@ -20,31 +18,55 @@ export default class FirebaseAdminService {
    *
    * @see https://firebase.google.com/docs/admin/setup#initialize-sdk
    */
-  private readonly CREDENTIALS_SCHEMA = firebaseAdminCredentials as admin.ServiceAccount
+  private serviceAccount: admin.ServiceAccount | undefined
 
-  /**
-   * The certificate that the Firebase Admin SDK will use to authenticate with the Firebase services. It is generated
-   * from the {@link CREDENTIALS_SCHEMA | credentials schema}.
-   */
-  private readonly CERTIFICATION = admin.credential.cert(this.CREDENTIALS_SCHEMA)
+  private async loadCredentials(): Promise<admin.ServiceAccount> {
+    try {
+      const credentialJSONFile = await import('./_/firebase-admin-credentials.json')
+      return credentialJSONFile as admin.ServiceAccount
+    } catch {
+      const credentialEnvironmentVariable = process.env.FIREBASE_ADMIN_CREDENTIAL_SCHEMA
+      return JSON.parse(credentialEnvironmentVariable) as admin.ServiceAccount
+    }
+  }
 
   /**
    * Connects to the Firebase services and returns the Firebase Admin SDK instance for the project.
+   *
+   * @see {@link admin.app.App}
    */
-  private connectToFirebase(): admin.app.App {
+  private async connectToFirebase(): Promise<admin.app.App> {
+    const credentials = await this.loadCredentials()
+
+    if (!this.serviceAccount) {
+      this.serviceAccount = credentials
+    }
+
+    // This is the certificate that the Firebase Admin SDK will use to authenticate with the Firebase services. It is
+    // generated from the `serviceAccount` object.
+    const certification = admin.credential.cert(credentials)
+
     return admin.initializeApp({
-      credential: this.CERTIFICATION,
+      credential: certification,
       databaseURL: this.REALTIME_DATABASE_URL,
     })
   }
 
   /**
-   * The various Firebase Admin SDK instances for the project.
+   * Creates the Firebase Admin SDK instance for the project. If the Firebase Admin SDK instance already exists, it will
+   * return the existing instance.
    */
-  private apps = getApps()
+  async singleton() {
+    // The various Firebase Admin SDK instances for the project. There's an issue open on Next.js's GitHub that
+    // recommends using a singleton for the Firebase Admin SDK instance. This is the singleton.
+    const apps = getApps()
 
-  /**
-   * The Firebase Admin SDK instance for the project.
-   */
-  public readonly instance = this.apps.length >= 1 ? this.apps[0] : this.connectToFirebase()
+    if (apps.length >= 1) {
+      return apps[0]
+    }
+
+    const instance = await this.connectToFirebase()
+
+    return instance
+  }
 }
--- src/services/firebase-client-service.ts
+++ src/services/firebase-client-service.ts
@@ -1,7 +1,4 @@
-import { type FirebaseApp, initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
-
-import firebaseClientCredentialConfig from './_/firebase-client-credential-config.json'
+import { type FirebaseApp, initializeApp, type FirebaseOptions } from 'firebase/app'
 
 /**
  * The service that provides access to the Firebase Admin SDK for the project to use on the client- and/or server-side
@@ -17,24 +14,34 @@ export default class FirebaseClientService {
    *
    * @see https://firebase.google.com/docs/web/setup#add-sdks-initialize
    */
-  private readonly FIREBASE_CONFIG = firebaseClientCredentialConfig
+  private readonly FIREBASE_CONFIG: FirebaseOptions | undefined
+
+  private async loadClientCredentialOptions(): Promise<FirebaseOptions> {
+    try {
+      const credentialJSONFile = await import('./_/firebase-client-credential-config.json')
+      return credentialJSONFile as FirebaseOptions
+    } catch {
+      const credentialEnvironmentVariable = process.env.NEXT_PUBLIC_FIREBASE_CLIENT_CREDENTIAL_SCHEMA
+      return JSON.parse(credentialEnvironmentVariable) as FirebaseOptions
+    }
+  }
 
   /**
    * Connects the application to Firebase and returns the Firebase Client SDK instance.
    */
-  private connect(): FirebaseApp {
-    return initializeApp(this.FIREBASE_CONFIG)
+  private async connect(): Promise<FirebaseApp> {
+    const options = await this.loadClientCredentialOptions()
+
+    return initializeApp(options)
   }
 
   // The singleton instance of the Firebase Client SDK.
   private $$singletonFirebaseClientInstance: FirebaseApp | null = null
 
-  public get instance(): FirebaseApp {
+  public async singleton(): Promise<FirebaseApp> {
     if (this.$$singletonFirebaseClientInstance === null) {
-      this.$$singletonFirebaseClientInstance = this.connect()
+      this.$$singletonFirebaseClientInstance = await this.connect()
     }
     return this.$$singletonFirebaseClientInstance
   }
-
-  public readonly auth = getAuth(this.instance)
 }
