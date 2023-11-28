e65e38103977fc429e21e42618e7b50413636a29
Leonardo Lemos <leo.lemos.ds@icloud.com>
2023-11-28 19:56:18 -0300

    refactor: modify "About Project" to avoid legal issues and replace Firebase mention with Clerk's

--- src/features/about-project/about-project.tsx
+++ src/features/about-project/about-project.tsx
@@ -7,21 +7,18 @@ import Paragraph from '@root/components/ui/paragraph'
 import ExternalLink from './components/external-link'
 import ThirdPartyPackages from './components/third-party-packages'
 
-const BEES_URL = 'https://bees.com/' as const
-const AB_INBEV_URL = 'https://ab-inbev.com/' as const
-
 function AboutProject(): JSX.Element {
   return (
     <main className='mx-auto mt-20 min-h-screen max-w-[96vw] md:max-w-4xl'>
       <Heading hierarchy='h1' className='my-5 text-center md:my-24'>
-        About P&P 🎉
+        About Planria 🎉
       </Heading>
       <Blockquote className='text-3xl'>We didn&rsquo;t find the perfect planning poker app, so we built it.</Blockquote>
       <Paragraph>
-        This quote is what we say when people ask us why we built P&P. But it&rsquo;s not the whole story. Our intention
-        with this quote is not to say that we&rsquo;re the first people to build a planning poker app. We&rsquo;re not.
-        Also, we don&rsquo;t want to say that we&rsquo;re the only people who can build a planning poker app.
-        We&rsquo;re not.&nbsp;
+        This quote is what we say when people ask us why we built Planria. But it&rsquo;s not the whole story. Our
+        intention with this quote is not to say that we&rsquo;re the first people to build a planning poker app.
+        We&rsquo;re not. Also, we don&rsquo;t want to say that we&rsquo;re the only people who can build a planning
+        poker app. We&rsquo;re not.&nbsp;
         <b>
           And most importantly, we don&rsquo;t want to say that there aren&rsquo;t any other planning poker apps out
           there that are better than ours
@@ -30,35 +27,25 @@ function AboutProject(): JSX.Element {
       </Paragraph>
       <Paragraph>
         What we want to say is that we&rsquo;re not aware of any other planning poker app that has the features that we
-        want. And that&rsquo;s why we built P&P. We built it for ourselves, and we&rsquo;re sharing it with you.
+        want. And that&rsquo;s why we built Planria. We built it for ourselves, and we&rsquo;re sharing it with you.
       </Paragraph>
       <Heading hierarchy='h2' className='mt-20 text-center'>
         Origin
       </Heading>
       <Paragraph>
-        Party&Planning - or as we like to call it, <i>P&P</i> - was born inside the Customer Membership Web Team
-        of&nbsp;
-        <Link href={BEES_URL} target='_blank' className='font-bold'>
-          BEES
-        </Link>
-        , the technology arm of the world&rsquo;s largest brewer,&nbsp;
-        <Link href={AB_INBEV_URL} target='_blank' className='font-bold'>
-          Anheuser-Busch InBev
-        </Link>
-        .
+        Planria was born inside one of the largest companies on Earth, but let&apos;s redact names for now.
       </Paragraph>
       <Paragraph>
-        As the name suggests, P&P may sound like &ldquo;plug&rdquo; and &ldquo;play&rdquo;, and that is the idea behind
-        it. We want to make it easy for you to start using P&P, and we want to make it easy for you to integrate P&P
-        into your existing workflow.
+        Planria has the idea idea behind of being a <b>plug-and-play</b> experience. We want to make it easy for you to
+        start using Planria, and we want to make it easy for you to integrate Planria into your existing workflow.
       </Paragraph>
       <Heading hierarchy='h2' className='mt-20 text-center'>
         Open Source & Your Data
       </Heading>
       <Paragraph>
         <b>Our commitment to keep the project as much open source as possible</b> is a way to ensure that you can use
-        P&P without any concerns about privacy or security. We want to make it easy for you to trust P&P. Also, we
-        declare everything that we do with the data that you input into P&P in our&nbsp;
+        Planria without any concerns about privacy or security. We want to make it easy for you to trust Planria. Also,
+        we declare everything that we do with the data that you input into Planria in our&nbsp;
         <Link href='/privacy-policy' target='_blank' className='font-bold'>
           Privacy Policy
         </Link>
@@ -79,15 +66,17 @@ function AboutProject(): JSX.Element {
         &nbsp;with all the security that they provide.
       </Paragraph>
       <Paragraph>
-        Your credentials (<i>i.e.</i> your email and password) are stored in the Firebase Authentication service, which
-        is managed by&nbsp;
-        <Link href='https://firebase.google.com/' className='font-bold'>
-          Google
+        Your credentials (<i>i.e.</i> your email and password) are stored in the Clerk Authentication service, which is
+        managed by&nbsp;
+        <Link href='https://clerk.com' className='font-bold'>
+          Clerk
         </Link>
         &nbsp;and is secured by them as you may find in their&nbsp;
-        <Link href='https://firebase.google.com/support/privacy' className='font-bold'>
-          Privacy and Security
+        <Link href='https://clerk.com/privacy' className='font-bold'>
+          Privacy Policy
         </Link>
+        and&nbsp;
+        <Link href='https://clerk.com/terms'>Terms & Conditions</Link>
         &nbsp;official documentation.
       </Paragraph>
       <ul className='ml-6 list-disc'>
@@ -96,11 +85,6 @@ function AboutProject(): JSX.Element {
         </li>
         <li>
           <ExternalLink href='https://vercel.com/docs/storage/vercel-postgres'>Vercel Postgres</ExternalLink>
-        </li>
-        <li>
-          <ExternalLink href='https://firebase.google.com/support/privacy'>
-            Privacy and Security in Firebase
-          </ExternalLink>
         </li>
       </ul>
       <Heading hierarchy='h3' className='mt-16 text-center'>
@@ -125,8 +109,8 @@ function AboutProject(): JSX.Element {
           shadcn/ui&nbsp;
           <span className='text-gray-500'>| Beautifully designed components built with Radix UI and Tailwind CSS.</span>
         </ExternalLink>
-        <ExternalLink href='https://firebase.google.com'>
-          Firebase&nbsp;<span className='text-gray-500'>| Google&apos;s Mobile and Web App Development Platform</span>
+        <ExternalLink href='https://clerk.com'>
+          Clerk&nbsp;<span className='text-gray-500'>| More than authentication. Complete user management.</span>
         </ExternalLink>
       </div>
       <Heading hierarchy='h3' className='mt-16 text-center'>
