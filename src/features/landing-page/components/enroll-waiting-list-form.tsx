9e451348786d6290b04b859fa412d1bc25211516
Leonardo Lemos <leo.lemos.ds@icloud.com>
2023-11-30 21:26:48 -0300

    refactor: rename P&P to "Planria" in `<EnrollWaitingListForm />`

--- src/features/landing-page/components/enroll-waiting-list-form.tsx
+++ src/features/landing-page/components/enroll-waiting-list-form.tsx
@@ -5,15 +5,16 @@ import Paragraph from '@root/components/ui/paragraph'
 
 import WaitingListFieldNames from '../constants/waiting-list-field-names'
 import enrollEmailToWaitingList from '../server-actions/enroll-email-to-waiting-list'
+
 import EnrollWaitingListSubmitButton from './enroll-waiting-list-submit-button'
 
 function EnrollWaitingListForm(): JSX.Element {
   return (
     <Fragment>
       <Paragraph className='md:max-w-[68rem]'>
-        As Party&Planning is still in beta, we are actively picking sign-ups to help us with feedback of the Party
-        Planner experience. If you would like to be notified when we open up to new users in this beta phase, please
-        sign up to our waiting list.
+        As Planria is still in beta, we are actively picking sign-ups to help us with feedback of the Party Planner
+        experience. If you would like to be notified when we open up to new users in this beta phase, please sign up to
+        our waiting list.
       </Paragraph>
       <form action={enrollEmailToWaitingList}>
         <div className='mt-4 flex items-center gap-2'>
