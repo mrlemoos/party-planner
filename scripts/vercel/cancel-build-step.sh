fa04ae815d3c6296f4ff6db95aba8545838552e2
Leonardo Lemos <leo.lemos.ds@icloud.com>
2023-12-27 19:50:30 -0300

    ci: validate BUILD CANCEL STEP based on the commit message, not the commit reference

--- scripts/vercel/cancel-build-step.sh
+++ scripts/vercel/cancel-build-step.sh
@@ -3,12 +3,15 @@
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 echo "VERCEL_GIT_COMMIT_MESSAGE: $VERCEL_GIT_COMMIT_MESSAGE"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == *"[ci skip]"  ]] ; then
-  # Proceed with the build
-  echo "🛑 - Build cancelled"
+# Cancel the build if the commit message contains "[ci skip]". This is useful when you want to skip a build for a 
+# specific commit.
+#
+# Reference:
+# https://vercel.com/guides/how-do-i-use-the-ignored-build-step-field-on-vercel#with-environment-variables
+if [[ "$VERCEL_GIT_COMMIT_MESSAGE" == *"[ci skip]"  ]] && [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]] ; then
+  echo "🛑 [CANCEL BUILD STEP] The build referred by $VERCEL_GIT_COMMIT_REF has been cancelled. See commit message: \"$VERCEL_GIT_COMMIT_MESSAGE\""
   exit 0;
-
 else
-  # Don't build
-  echo "✅ - Build can proceed"
+  echo "✅ [CANCEL BUILD STEP] The build referred by $VERCEL_GIT_COMMIT_MESSAGE is allowed to proceed"
+  exit 1;
 fi
