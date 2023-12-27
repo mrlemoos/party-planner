3fbc116d64f34f77669f68ecc63f6cf0ce038263
Leonardo Lemos <leo.lemos.ds@icloud.com>
2023-12-27 19:58:03 -0300

    refactor: delete `import-jira-issues` feature

--- src/features/import-jira-issues/import-jira-issues.tsx
+++
@@ -1,7 +0,0 @@
-import ImportSpreadsheet from './components/import-spreadsheet'
-
-function ImportJiraIssues(): JSX.Element {
-  return <ImportSpreadsheet onImport={function () {}} />
-}
-
-export default ImportJiraIssues
