b69da78339039875a73a279b294beddba90d46bd
Leonardo Lemos <leo.lemos.ds@icloud.com>
2023-12-28 22:04:59 -0300

    refactor: fallback `Inter` font to Apple system fonts, Roboto, and Open Sans

--- src/styles/fonts/font-sans.ts
+++ src/styles/fonts/font-sans.ts
@@ -4,6 +4,7 @@ const FontSans = Inter({
   subsets: ['latin'],
   variable: '--font-sans',
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  fallback: ['-apple-system', 'BlinkMacSystemFont', 'Roboto', 'Open Sans', 'sans-serif'],
 })
 
 export default FontSans
