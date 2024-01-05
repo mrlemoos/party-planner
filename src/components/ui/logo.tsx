7740d3704a49ebab3879c3e6d45dea4f62634302
Leonardo Lemos <leo.lemos.ds@icloud.com>
2024-01-04 21:39:09 -0300

    refactor: forward Logo component's ref to root `<div>` element

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,6 +1,13 @@
+'use client'
+
+import { forwardRef } from 'react'
+
 import FontLogo from '@root/styles/fonts/font-logo'
 import merge from '@root/util/merge'
 
+/**
+ * The props for the {@link Logo} component.
+ */
 interface LogoProps {
   /**
    * The class name that is merged with the default class name at the root element.
@@ -9,23 +16,29 @@ interface LogoProps {
 }
 
 /**
+ * The reference type for the {@link Logo} component.
+ */
+type LogoReferenceType = HTMLDivElement
+
+/**
  * The logo of the application.
  *
  * @props {@link LogoProps}
+ * @ref {@link LogoReferenceType}
  */
-function Logo({ className }: LogoProps): JSX.Element {
-  return (
-    <div
-      className={merge(
-        'flex h-7 w-7 items-center justify-center rounded-lg bg-foreground font-semibold text-background',
-        FontLogo.className,
-        className,
-      )}
-      aria-label='Planria'
-    >
-      <span>P</span>
-    </div>
-  )
-}
+const Logo = forwardRef<LogoReferenceType, LogoProps>(({ className }, ref) => (
+  <div
+    ref={ref}
+    className={merge(
+      'flex h-7 w-7 items-center justify-center rounded-lg bg-foreground font-semibold text-background',
+      FontLogo.className,
+      className,
+    )}
+    aria-label='Planria'
+  >
+    <span>P</span>
+  </div>
+))
+Logo.displayName = 'Logo'
 
 export default Logo
